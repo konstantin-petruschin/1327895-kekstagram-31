656de40eea37256b6c33337e8d3a925ec53c2d3f
konstantin-petruschin <konstantin-petruschin1988mail.ru>
2024-03-20 13:38:58 +0300

    8.14. Открывается и закрывается

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import {similarPhoto} from './data.js';
-import {renderSimilarPhotoPostsArray} from './create-posts.js';
-
+import {renderSimilarPhotoPostsArray} from './similar-posts.js';
+import './user-modal.js';
 const similarPhotoPostsArray = similarPhoto();
 
 renderSimilarPhotoPostsArray(similarPhotoPostsArray);
--- js/user-modal.js
+++ js/user-modal.js
@@ -1,4 +1,31 @@
-import './create-posts.js';
+import { isEscapeKey } from './util';
+import { similarPhotoPostsArray } from './data';
+const userPictureElement = document.querySelector('.big-picture');
+const userPictureCloseElement = document.querySelector('.big-picture__cancel');
+userPictureElement.classList.remove('hidden');
 
-const userModalElement = document.querySelector('.big-picture');
-userModalElement.classList.remove('hidden');
+userPictureElement.addEventListener('click', (evt) => {
+  if (evt.target.closest('.big-picture')) {
+    userPictureElement.classList.remove('hidden');
+    similarPhotoPostsArray.forEach((photo, id) => {const pictureBigImg = userPictureElement.querySelector('.big-picture__img');
+      pictureBigImg.src = photo.url;
+      pictureBigImg.alt = photo.description;
+      userPictureElement.querySelector('.picture__likes').textContent = photo.likes;
+      userPictureElement.querySelector('.picture__comments').textContent = photo.comments.length;
+      userPictureElement.dataset.id = id;
+      userPictureElement.appendChild(userPictureElement);
+    });
+  }
+});
+
+userPictureCloseElement.addEventListener('click', () =>{
+  userPictureElement.classList.add('hidden');
+});
+
+document.addEventListener('keydown' ,(evt)=> {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    userPictureElement.classList.add('hidden');
+  }
+});
+
