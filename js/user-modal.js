639e64d79a9e8064900848372eb8bdad3fe9c6b0
konstantin-petruschin <konstantin-petruschin1988mail.ru>
2024-03-23 00:34:05 +0300

    Открывается и закрывается. часть 1

--- js/user-modal.js
+++ js/user-modal.js
@@ -1,34 +1,54 @@
 import { isEscapeKey } from './util.js';
 
-const bigPictureElement = document.querySelector('big-picture');
-const allPictureElement = document.querySelector('pictures');
+const bigPictureElement = document.querySelector('.big-picture');
+const allPictureElement = document.querySelector('.pictures');
 const userPictureCloseElement = document.querySelector('.big-picture__cancel');
 
-const showBigPicture = (arr) => {
+const showBigPicture = (photoPosts) => {
   allPictureElement.addEventListener('click', (evt) => {
-    if (evt.target.closest('.big-picture')) {
+    if (evt.target.closest('.picture')) {
+      const photo = photoPosts[parseInt(evt.target.closest('.picture').dataset.id, 10)];
+
       bigPictureElement.classList.remove('hidden');
-      //   const pictureBigImg = bigPictureElement.querySelector('.big-picture__img img');
-      //   pictureBigImg.src = photo.url;
-      //   pictureBigImg.alt = photo.description;
-      //   bigPictureElement.querySelector('.picture__likes').textContent = photo.likes;
-      //   bigPictureElement.querySelector('.picture__comments').textContent = photo.comments.length;
+      bigPictureElement.querySelector('.big-picture__img img').src = photo.url;
+      bigPictureElement.querySelector('.likes-count').textContent = photo.likes;
+      bigPictureElement.querySelector('.social__comment-shown-count').textContent = photo.comments.length;
+      bigPictureElement.querySelector('.social__comment-total-count').textContent = photo.comments.length;
+
+      const socialCommentsList = bigPictureElement.querySelector('.social__comments');
+      bigPictureElement.querySelector('.social__caption').textContent = photo.description;
+      socialCommentsList.innerHTML = '';
+
+      const socialCommentTemplate = bigPictureElement.querySelector('.social__comment').cloneNode(true);
+      const socialCommentsFragment = document.createDocumentFragment();
+
+      photo.comments.forEach((comment) => {
+        const newComment = socialCommentTemplate.cloneNode(true);
+        newComment.querySelector('.social__picture').src = comment.avatar;
+        newComment.querySelector('.social__picture').alt = comment.name;
+        newComment.querySelector('.social__text').textContent = comment.message;
+        socialCommentsFragment.appendChild(newComment);
+      });
+
+      socialCommentsList.appendChild(socialCommentsFragment);
+      bigPictureElement.querySelector('.social__comment-count').classList.add('hidden');
+      bigPictureElement.querySelector('.comments-loader').classList.add('hidden');
+
+      document.body.classList.add('modal-open');
     }
   });
 };
 
-
-// evt.target.closest('.big-picture') - с этого элемента получить дата-атрибут
-// обратиться к массиву arr (и переименовать его) и в нем найти тот объект у которого такой же ай ди;
-// у нас нет объекта фото достать из массива evt.target.closest('.big-picture')
-userPictureCloseElement.addEventListener('click', () =>{
+userPictureCloseElement.addEventListener('click', () => {
   bigPictureElement.classList.add('hidden');
+  document.body.classList.remove('modal-open');
 });
 
-document.addEventListener('keydown' ,(evt)=> {
+document.addEventListener('keydown', (evt) => {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
     bigPictureElement.classList.add('hidden');
+    document.body.classList.remove('modal-open');
   }
 });
 
