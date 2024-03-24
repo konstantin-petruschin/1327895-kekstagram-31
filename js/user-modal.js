e7bb3b847224ac8b1f9010676813bcf8b9808a27
konstantin-petruschin <konstantin-petruschin1988mail.ru>
2024-03-24 22:44:20 +0300

    Открывается и закрывается. Вторая часть на проверку

--- js/user-modal.js
+++ js/user-modal.js
@@ -1,4 +1,5 @@
 import { isEscapeKey } from './util.js';
+import {renderComments, clearComments } from './render-comments.js';
 
 const bigPictureElement = document.querySelector('.big-picture');
 const allPictureElement = document.querySelector('.pictures');
@@ -9,30 +10,15 @@ const showBigPicture = (photoPosts) => {
     if (evt.target.closest('.picture')) {
       const photo = photoPosts.find((item) => item.id === parseInt(evt.target.closest('.picture').dataset.id, 10));
 
+      clearComments();
+      renderComments(photo.comments);
+
       bigPictureElement.classList.remove('hidden');
       bigPictureElement.querySelector('.big-picture__img img').src = photo.url;
       bigPictureElement.querySelector('.likes-count').textContent = photo.likes;
       bigPictureElement.querySelector('.social__comment-shown-count').textContent = photo.comments.length;
       bigPictureElement.querySelector('.social__comment-total-count').textContent = photo.comments.length;
-
-      const socialCommentsList = bigPictureElement.querySelector('.social__comments');
       bigPictureElement.querySelector('.social__caption').textContent = photo.description;
-      socialCommentsList.innerHTML = '';
-
-      const socialCommentTemplate = bigPictureElement.querySelector('.social__comments').cloneNode(true);
-      const socialCommentsFragment = document.createDocumentFragment();
-
-      photo.comments.forEach((comment) => {
-        const newComment = socialCommentTemplate.cloneNode(true);
-        newComment.querySelector('.social__picture').src = comment.avatar;
-        newComment.querySelector('.social__picture').alt = comment.name;
-        newComment.querySelector('.social__text').textContent = comment.message;
-        socialCommentsFragment.appendChild(newComment);
-      });
-
-      socialCommentsList.appendChild(socialCommentsFragment);
-      bigPictureElement.querySelector('.social__comment-count').classList.add('hidden');
-      bigPictureElement.querySelector('.comments-loader').classList.add('hidden');
 
       document.body.classList.add('modal-open');
     }
