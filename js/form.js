3906fc224f4c43fd01a7246fee2c7b85a85ff185
konstantin-petruschin <konstantin-petruschin1988mail.ru>
2024-03-28 19:38:40 +0300

    Правда или действие

--- js/form.js
+++ js/form.js
@@ -2,9 +2,12 @@ import { isEscapeKey } from './util.js';
 
 const uploadForm = document.querySelector('.img-upload__form');
 const pageBody = document.querySelector('body');
-const imageUploadInput = uploadForm.querySelector('.img-upload__input'); //#upload-file
+const imageUploadInput = uploadForm.querySelector('.img-upload__input');
 const imageUploadOverlay = uploadForm.querySelector('.img-upload__overlay');
-const imageUploadCancel = uploadForm.querySelector('.img-upload__cancel'); //#upload-cancel
+const imageUploadCancel = uploadForm.querySelector('.img-upload__cancel');
+
+const hashtagInput = uploadForm.querySelector('.text__hashtags');
+const commentInput = uploadForm.querySelector('.text__description');
 
 const openUserModal = () => {
   imageUploadInput.addEventListener('change', () => {
@@ -12,7 +15,6 @@ const openUserModal = () => {
     pageBody.classList.add('modal-open');
     imageUploadInput.value = '';
 
-    // Слушаем событие клика на кнопке "Отмена"
     imageUploadCancel.addEventListener('click', () => {
       imageUploadOverlay.classList.add('hidden');
       pageBody.classList.remove('modal-open');
@@ -26,6 +28,18 @@ document.addEventListener('keydown', (evt) => {
     imageUploadOverlay.classList.add('hidden');
     pageBody.classList.remove('modal-open');
   }
+});
+
+
+const pristine = new Pristine(uploadForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorClass: 'img-upload__field-wrapper--error',
+  errorTextParrent: 'img-upload__field-wrapper',
+
+});
+pristine.addValidator(hashtagInput, (value) => {
+  const hasNumber = /^#[a-zа-яё0-9]{1,19}$/i.test(value);
+  return hasNumber;
 });
 
 openUserModal();
