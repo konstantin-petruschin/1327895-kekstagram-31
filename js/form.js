f35cd790792aa9ee3ddd8d2f1e06b2061b15425f
konstantin-petruschin <konstantin-petruschin1988mail.ru>
2024-04-04 01:28:06 +0300

    Выполнение домашнего задания правда или действие

--- js/form.js
+++ js/form.js
@@ -1,22 +1,18 @@
 import { isEscapeKey } from './util.js';
 
+const MAX_COUNT_HASHTAGS = 5;
+const MAX_LENGTH_COMMENT = 140;
+
 const uploadForm = document.querySelector('.img-upload__form');
 const pageBody = document.querySelector('body');
-const imageUploadInput = uploadForm.querySelector('.img-upload__input');
-const imageUploadOverlay = uploadForm.querySelector('.img-upload__overlay');
-const imageUploadCancel = uploadForm.querySelector('.img-upload__cancel');
+const imageUploadInput = uploadForm.querySelector('.img-upload__input'); // uploadFileControl
+const imageUploadOverlay = uploadForm.querySelector('.img-upload__overlay'); //PhotoEditorForm
+const imageUploadCancel = uploadForm.querySelector('.img-upload__cancel'); //photoEditorResetBtn
 
 const hashtagInput = uploadForm.querySelector('.text__hashtags');
 const commentInput = uploadForm.querySelector('.text__description');
 
-const isFieldFocused = () => document.activeElement === hashtagInput || document.activeElement === commentInput;
-
-const clearErrorMsg = () => {
-  const existingError = hashtagInput.parentNode.querySelector('.error-message');
-  if (existingError) {
-    existingError.remove();
-  }
-};
+let errorMessage = '';
 
 const openUserModal = () => {
   imageUploadInput.addEventListener('change', () => {
@@ -31,50 +27,124 @@ const openUserModal = () => {
   });
 };
 
-document.addEventListener('keydown', (evt) => {
-  if (isEscapeKey(evt) && !isFieldFocused()) {
+
+const onPhotoEditorResetButtonClick = () => closePhotoEditor() ;
+const onDocunentKeydown = (evt) => {
+  if(isEscapeKey(evt)) {
     evt.preventDefault();
-    imageUploadOverlay.classList.add('hidden');
-    pageBody.classList.remove('modal-open');
-  }
-});
-
-  const pristine = new Pristine(uploadForm, {
-    classTo: 'img-upload__field-wrapper',
-    errorTextClass: 'img-upload__field-wrapper--error',
-    errorTextParent: 'img-upload__field-wrapper',
-  });
-
-  const checkLengthComment = () => commentInput.value.length <= 140;
-  const errorMessage = `Длина комментария ${commentInput.value.length} больше 140 символов`;
-  pristine.addValidator(commentInput, checkLengthComment, errorMessage);
-
-  const hashtagRegex = /^#[a-zA-Z0-9]{1,19}( #[a-zA-Z0-9]{1,19})*$/;
-  const checkValidHashtags = () => hashtagRegex.test(hashtagInput.value);
-  const checkPresenceGaps = () => hashtagInput.value.replace(/\s{2,}/g, ' ');
-  const hashtagsArray = hashtagInput.value.trim().toLowerCase().split(' ');
-  const checkQuantityHashtags = () => hashtagsArray.length < 5;
-  const checkHashtagDuplicate = () => new Set(hashtagsArray).size === hashtagsArray.length;
-
-  if (hashtagInput.value) {
-    pristine.addValidator(hashtagInput, checkValidHashtags, 'Хештеги должны начинаться с символа #, состоять из букв и цифр, и иметь длину от 1 до 19 символов');
-    pristine.addValidator(hashtagInput, checkPresenceGaps, 'Разделите хештеги через пробел');
-    pristine.addValidator(hashtagInput, checkQuantityHashtags, 'Укажите не больше пяти хештегов');
-    pristine.addValidator(hashtagInput, checkHashtagDuplicate, 'Один и тот же хештег не может быть использован дважды');
-  }
-
-  const isValid = pristine.validate();
-
-  // Define a function to handle the form submission event
-const handleFormSubmission = (event) => {
-  if (!isValid) {
-    event.preventDefault();
+    if(document.activeElement === hashtagInput || document.activeElement === commentInput) {
+      evt.stopPropagation();
+    } else {
+      uploadForm.reset();
+      closePhotoEditor();
+    }
   }
 };
 
-// Add event listener for form submission (assuming it's a 'submit' event)
-uploadForm.addEventListener('submit', handleFormSubmission);
+function closePhotoEditor () {
+  imageUploadOverlay.classList.add('hidden');
+  pageBody.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocunentKeydown);
+  imageUploadCancel.removeEventListener('click', onPhotoEditorResetButtonClick);
+  imageUploadInput.value = '';
+}
+
+export const initUploadModal = () => {
+  imageUploadInput.addEventListener('change', () =>{
+    imageUploadOverlay.classList.remove('hidden');
+    pageBody.classList.add('modal-open');
+    imageUploadCancel.addEventListener('cklick', onPhotoEditorResetButtonClick);
+    document.addEventListener('keydown' , onDocunentKeydown);
+  });
+};
+
+const pristine = new Pristine(uploadForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper--error',
+  errorTextParent: 'img-upload__field-wrapper',
+});
+
+const error = () => errorMessage;
+const isValidHashtags = (value) => {
+  errorMessage = '';
+
+  const inputText = value.toLowerCase().trim();
+
+  if(!inputText.length === 0) {
+    return true;
+  }
+
+  const inputArray = inputText.split(/\s+/);
+
+  const hashtagRegex = /^#[a-zа-яё0-9]{1,19}$/i;
+  const conditionsArray = [
+    {
+      check: inputArray.some((item) => !hashtagRegex.test(item)),
+      error: 'Хештеги должны начинаться с символа #, состоять из букв и цифр, и иметь длину от 1 до 19 символов',
+    },
+    {
+      check: inputArray.length > MAX_COUNT_HASHTAGS,
+      error: 'Укажите не больше пяти хештегов',
+    },
+    {
+      check: inputArray.some((item, num, array) => array.includes(item, num + 1)),
+      error: 'Один и тот же хештег не может быть использован дважды',
+    },
+
+  ];
+
+  return conditionsArray.every((itemCondition) => {
+    const isValid = itemCondition.check;
+    if (isValid) {
+      errorMessage = itemCondition.error;
+    }
+    return !isValid;
+  });
+};
+
+const onHashtagInput = () => isValidHashtags(hashtagInput.value);
+
+const onFormSubmit = (evt) => {
+  evt.preventDefault();
+  if (pristine.validate()) {
+    hashtagInput.value = hashtagInput.value.trim().replaceAll(/\s+/g, ' ');
+    uploadForm.submit();
+  }
+};
 
 
+// const hashtagsArray = (value) => value.trim().toLowerCase().split(' ');
 
+// const checkValidHashtags = (value) => hashtagRegex.test(value);
+// const checkQuantityHashtags = (value) => hashtagsArray(value).length < 5;
+// const checkHashtagDuplicate = () => new Set(hashtagsArray).size === hashtagsArray.length;
+// checkValidHashtags();
+// if (hashtagInput.value) {
+//   pristine.addValidator(hashtagInput, checkValidHashtags, );
+//   pristine.addValidator(hashtagInput, checkQuantityHashtags, 'Укажите не больше пяти хештегов');
+//   pristine.addValidator(hashtagInput, checkHashtagDuplicate, );
+// }
+
+
+// document.addEventListener('keydown', (evt) => {
+//   if (isEscapeKey(evt) && !isFieldFocused()) {
+//     evt.preventDefault();
+//     imageUploadOverlay.classList.add('hidden');
+//     pageBody.classList.remove('modal-open');
+//   }
+// });
+
+const checkLengthComment = () => commentInput.value.length <= MAX_LENGTH_COMMENT;
+errorMessage = `Длина комментари больше ${MAX_LENGTH_COMMENT} символов`;
+
+uploadForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
+
+pristine.addValidator(hashtagInput, isValidHashtags, error, 2, false);
+pristine.addValidator(commentInput, checkLengthComment, errorMessage);
 openUserModal();
+
+hashtagInput.addEventListener('input', onHashtagInput);
+uploadForm.addEventListener('submit', onFormSubmit);
