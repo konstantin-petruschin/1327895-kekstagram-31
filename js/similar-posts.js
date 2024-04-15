d329b979c2585e629465b4735f716ce35d21cfe6
konstantin-petruschin <konstantin-petruschin1988mail.ru>
2024-04-15 22:03:07 +0300

    исправляет ошибки

--- js/similar-posts.js
+++ js/similar-posts.js
@@ -2,8 +2,14 @@ const picturesTemplate = document.querySelector('#picture').content.querySelecto
 const picturesContainer = document.querySelector('.pictures');
 const listFragmentPhoto = document.createDocumentFragment();
 
+const removeElements = (removedElements) => {
+  const elementsToRemove = document.querySelectorAll(removedElements);
+  elementsToRemove.forEach((element) => element.remove());
+};
+
 const renderSimilarPhotoPostsArray = (similarPhotoPostsArray) => {
-  picturesContainer.innerHTML = '';
+  removeElements('.pictures .picture');
+
   similarPhotoPostsArray.forEach((photo) => {
     const pictureElement = picturesTemplate.cloneNode(true);
     const pictureImg = pictureElement.querySelector('.picture__img');
@@ -18,4 +24,4 @@ const renderSimilarPhotoPostsArray = (similarPhotoPostsArray) => {
   picturesContainer.appendChild(listFragmentPhoto);
 };
 
-export {renderSimilarPhotoPostsArray};
+export { renderSimilarPhotoPostsArray };
